e746bee62351a75bdf38b15022515bf939b9ee5d
mbonesgaming <mbonesgaming@gmail.com>
2024-11-06 15:42:49 -0600

    formatting

--- src/form/TechRequestDialog.tsx
+++ src/form/TechRequestDialog.tsx
@@ -1,38 +1,35 @@
-import { DialogContent } from '@mui/material';
-import {Dialog} from '@mui/material';
-import DialogTitle from '@mui/material/DialogTitle';
-import { useState, useEffect } from 'react';
-import { getTechRequests } from '../apiService/techRequestService';
-import TechRequestForm from './TechRequestForm';
+import { DialogContent } from "@mui/material";
+import { Dialog } from "@mui/material";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useState, useEffect } from "react";
+import { getTechRequests } from "../apiService/techRequestService";
+import TechRequestForm from "./TechRequestForm";
 
 interface TechRequestDialogProps {
-    open: boolean;
-    selectedValue?: string;
-    onClose?: (value: string) => void;
-    handleCloseDialog?: () => void;
-};
+  open: boolean;
+  selectedValue?: string;
+  onClose?: (value: string) => void;
+  handleCloseDialog?: () => void;
+}
 
-  export interface TechnicalRequest {
-    id: number;
-    email: string;
-    description: string;
-    dueDate: any;
-  } 
-  
+export interface TechnicalRequest {
+  id: number;
+  email: string;
+  description: string;
+  dueDate: any;
+}
+
 const TechRequestDialog = (props: TechRequestDialogProps) => {
+  const { open, handleCloseDialog } = props;
 
-    const {open, handleCloseDialog} = props;
-
-
-    return(
-        <Dialog open={open} onClose={handleCloseDialog} maxWidth='sm' fullWidth>
-            <DialogTitle>Technical Request Form</DialogTitle>
-            <DialogContent>
-    <TechRequestForm handleCloseDialog={handleCloseDialog}/>
-            </DialogContent>
-        </Dialog>
-     
-    )
+  return (
+    <Dialog open={open} onClose={handleCloseDialog} maxWidth="sm" fullWidth>
+      <DialogTitle>Technical Request Form</DialogTitle>
+      <DialogContent>
+        <TechRequestForm handleCloseDialog={handleCloseDialog} />
+      </DialogContent>
+    </Dialog>
+  );
 };
 
-export default TechRequestDialog; 
+export default TechRequestDialog;
--- src/form/TechRequestForm.tsx
+++ src/form/TechRequestForm.tsx
@@ -1,115 +1,120 @@
 import { Button, DialogActions, FormControl, TextField } from "@mui/material";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs, { Dayjs } from 'dayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 import { useState } from "react";
 import { addTechRequest } from "../apiService/techRequestService";
 
 export interface TechnicalRequest {
-    id: number;
-    email: string;
-    description: string;
-    dueDate: any;
-  } 
+  id: number;
+  email: string;
+  description: string;
+  dueDate: any;
+}
 
-  interface TechRequestDialogProps {
-    open?: boolean;
-    selectedValue?: string;
-    onClose?: (value: string) => void;
-    handleCloseDialog?: () => void;
-};
+interface TechRequestDialogProps {
+  open?: boolean;
+  selectedValue?: string;
+  onClose?: (value: string) => void;
+  handleCloseDialog?: () => void;
+}
 
 const TechRequestForm = (props: TechRequestDialogProps) => {
-    const { handleCloseDialog } = props;
-    const [email, setEmail] = useState('');
-    const [description, setDescription] = useState('');
-    const [dueDate, setDueDate] = useState<Dayjs | null>(dayjs());
-    const [emailError, setEmailError] = useState(false);
-    const [descriptionError, setDescriptionError] = useState(false);
-    const [dueDateError, setDueDateError] = useState(false);
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    const yearAhead = dayjs().add(1, 'year');
+  const { handleCloseDialog } = props;
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState<Dayjs | null>(dayjs());
+  const [emailError, setEmailError] = useState(false);
+  const [descriptionError, setDescriptionError] = useState(false);
+  const [dueDateError, setDueDateError] = useState(false);
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  const yearAhead = dayjs().add(1, "year");
 
-
-    const handleEmail = (e: any) => {
-        setEmail(e.target.value)
-        const validateEmail = emailRegex.test(email);
-        if(validateEmail){
-            setEmailError(false);
-        }
-        else{
-            setEmailError(true);
-        }
+  const handleEmail = (e: any) => {
+    setEmail(e.target.value);
+    const validateEmail = emailRegex.test(email);
+    if (validateEmail) {
+      setEmailError(false);
+    } else {
+      setEmailError(true);
     }
+  };
 
-    const handleDescription = (e: any) => {
-        setDescription(e.target.value)
-        const validateDescription = description.length < 1000 || description.length > 100
-        console.log(validateDescription, 'val desc')
-        if(validateDescription){
-            setDescriptionError(false);
-        }
-        else{
-            setDescriptionError(true);
-        }
+  const handleDescription = (e: any) => {
+    setDescription(e.target.value);
+    const validateDescription =
+      description.length < 1000 || description.length > 100;
+    console.log(validateDescription, "val desc");
+    if (validateDescription) {
+      setDescriptionError(false);
+    } else {
+      setDescriptionError(true);
     }
+  };
 
-    const addTechnicalRequest = () => {
-        const techRequest: TechnicalRequest = {
-            email,
-            description,
-            dueDate,
-            id: Math.floor(Math.random() * 10)
-        }
-        console.log(techRequest, 'tech request')
-        addTechRequest(techRequest);
+  const addTechnicalRequest = () => {
+    const techRequest: TechnicalRequest = {
+      email,
+      description,
+      dueDate,
+      id: Math.floor(Math.random() * 10),
     };
+    console.log(techRequest, "tech request");
+    addTechRequest(techRequest);
+  };
 
-    return(
-        <div>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-           <form>
-        <FormControl>
-            <TextField label='Email' variant='outlined' required 
-            helperText={emailError ? 'Email must be valid and include @ sign.' : ''} 
-            value={email}
-            error={emailError} 
-            onChange={handleEmail}
+  return (
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <form>
+          <FormControl>
+            <TextField
+              label="Email"
+              variant="outlined"
+              required
+              helperText={
+                emailError ? "Email must be valid and include @ sign." : ""
+              }
+              value={email}
+              error={emailError}
+              onChange={handleEmail}
             />
-        </FormControl>
-        <FormControl>
-            <TextField label='Description' variant='outlined' 
-            required 
-            multiline 
-            helperText={descriptionError ? 'Description must be between 100 andd 1000 characters.' : ''} 
-            value={description}
-            
-            onChange={handleDescription}
+          </FormControl>
+          <FormControl>
+            <TextField
+              label="Description"
+              variant="outlined"
+              required
+              multiline
+              helperText={
+                descriptionError
+                  ? "Description must be between 100 andd 1000 characters."
+                  : ""
+              }
+              value={description}
+              onChange={handleDescription}
             />
-            </FormControl>
-            <FormControl>
-            <DatePicker 
-            label="Due Date" 
-            disablePast
-            maxDate={yearAhead}
-            
-            defaultValue={dayjs()}
-            onChange={(e) => 
-            setDueDate(e)}
-            format="YYYY-MM-DD"/>
-    
-</FormControl>
-           </form>
-           <DialogActions>
-            <Button onClick={handleCloseDialog}>Cancel</Button>
-            <Button onClick={addTechnicalRequest}>Submit</Button>
-           </DialogActions>
-           </LocalizationProvider>
-        </div>
-    )
-}
+          </FormControl>
+          <FormControl>
+            <DatePicker
+              label="Due Date"
+              disablePast
+              maxDate={yearAhead}
+              defaultValue={dayjs()}
+              onChange={(e) => setDueDate(e)}
+              format="YYYY-MM-DD"
+            />
+          </FormControl>
+        </form>
+        <DialogActions>
+          <Button onClick={handleCloseDialog}>Cancel</Button>
+          <Button onClick={addTechnicalRequest}>Submit</Button>
+        </DialogActions>
+      </LocalizationProvider>
+    </div>
+  );
+};
 
 export default TechRequestForm;
