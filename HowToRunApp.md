a024583aa6f1b4f3f694138d1c5dbd584806387d
mbonesgaming <mbonesgaming@gmail.com>
2024-11-06 15:35:54 -0600

    initial setup, apiService

--- HowToRunApp.md
+++ HowToRunApp.md
@@ -11,6 +11,20 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 # This app contains:
  node, react MUI components
 
- To install MUI: npm install @mui/material @emotion/react @emotion/styled
+ # To install MUI:
+  npm install @mui/material @emotion/react @emotion/styled
 
  Link to MUI: https://mui.com/material-ui/
+
+# Install PrimeReact: npm install primereact 
+Link to PrimeReact: https://primereact.org/installation/
+
+# Install MUI Date pickers: 
+npm install @mui/x-date-pickers
+
+npm install dayjs
+
+Link to install date pickers: https://mui.com/x/react-date-pickers/getting-started/
+
+# Install axios for http requests
+npm install axios
---
+++ src/apiService/techRequestService.tsx
@@ -0,0 +1,113 @@
+import axios from 'axios';
+
+const uri = 'https://localhost:7163/api/technical-request';
+
+const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'http://localhost:3000'
+  }
+
+  const api = axios.create({
+    baseURL: uri
+  });
+
+  export interface TechnicalRequest {
+    id: number;
+    email: string;
+    description: string;
+    dueDate: Date;
+  }
+
+
+
+// export const getTechRequests = async () => {
+//     await fetch(url, {
+//         method: 'GET',
+//         headers,
+//       })
+//         .then((res) => {
+//           console.log(res);
+//           return res.json();
+//         })
+//         .then((data: TechnicalRequest[] )=>{
+//           console.log(data);
+//           return data;
+//         })
+//         .catch(error => console.error('Unable to retrieve data', error))
+// }
+
+export const getTechRequests = async () => {
+  return await api.get('').then(
+  (response)=> {
+    console.log(response);
+    return response;
+  })};
+
+export const addTechRequest = async (body: any) => {
+    return await fetch(uri, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(body)
+      })
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then((data)=>{
+          console.log(data);
+          return data;
+        })
+        .catch(error => console.error('Unable to add request', error));
+}
+
+export const deleteTechRequest = (id: number) => {
+    return fetch(`${uri}/${id}`, {
+        method: 'DELETE',
+      })
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then((data)=>{
+          console.log(data);
+          return data;
+        })
+        .catch(error => console.error('Unable to delete request', error));
+}
+
+
+export const updateTechRequest = async (body: any, id: number) => {
+    return await fetch(`${uri}/${id}`, {
+        method: 'PUT',
+        headers,
+        body: JSON.stringify(body)
+      })
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then((data)=>{
+          console.log(data);
+          return data;
+        })
+        .catch(error => console.error('Unable to update request', error));
+}
+
+export const getTechRequestsById = async (id: number) => {
+  return await fetch(`${uri}/${id}`, {
+        method: 'GET',
+        headers,
+      })
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then((data)=>{
+          console.log(data);
+          return data;
+        })
+        .catch(error => console.error('Unable to retrieve data for this id', error))
+}
+
+
